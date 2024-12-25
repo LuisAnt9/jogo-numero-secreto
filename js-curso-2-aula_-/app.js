45358a348d3fd92808106f3fb8874bcc5f8607ff
Luís Antônio <luisan793@gmail.com>
2024-12-25 14:21:01 -0300

    projeto inicial

---
+++ js-curso-2-aula_-/app.js
@@ -0,0 +1,67 @@
+let listaDeNumerosSorteados = []
+let numeroLimite = 100
+let numeroSecreto = gerarNumeroAleatorio()
+let tentativas = 1
+
+
+function exibirTexto(tag, texto) {
+    let campo = document.querySelector(tag)
+    campo.innerHTML = texto;
+}
+
+function mensagemInicial() {
+    exibirTexto('h1', "Jogo do número secreto")
+    exibirTexto('p', 'Escolha um número entre 1 e 100')
+}
+
+mensagemInicial()
+
+function verificarChute() {
+    let chute = document.querySelector('input').value
+
+if (chute == numeroSecreto) {
+    exibirTexto('h1', 'Acertou!')
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+    let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa} !`
+    exibirTexto('p', mensagemTentativas)
+    document.getElementById('reiniciar').removeAttribute('disabled')
+} else {
+    if(chute > numeroSecreto) {
+        exibirTexto('p', 'O número secreto é menor')
+    } else {
+        exibirTexto('p', 'O número secreto é maior')
+    }
+    tentativas ++
+    limparCampo()
+}
+    console.log(chute == numeroSecreto)
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1)
+    let quantDeElementosNaLista = listaDeNumerosSorteados.length
+
+    if(quantDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = []
+    }
+
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio()
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido)
+        console.log(listaDeNumerosSorteados)
+        return numeroEscolhido
+    }
+}
+
+function limparCampo(){
+    chute = document.querySelector('input')
+    chute.value = ''
+}
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio()
+    limparCampo()
+    tentativas = 1
+    mensagemInicial()
+    document.getElementById('reiniciar').setAttribute('disabled', true)
+}
